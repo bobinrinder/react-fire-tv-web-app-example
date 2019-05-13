b9f54079e5e6f5d50257c4639f5c6f2380a74c9b
bobinrinder <robin@m56studios.com>
2019-05-13 16:38:33 -0700

    added basic example code

--- src/App.js
+++ src/App.js
@@ -1,23 +1,67 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useCallback } from 'react';
 import './App.css';
+import { useKeyUp, useKeyRepeat } from 'react-keyboard-input-hook';
 
 function App() {
+  const [selectedIndex, setSelectedIndex] = useState(0);
+
+  const handleKeyUp = useCallback(
+    ({ keyName }) => {
+      if (keyName === 'ArrowDown') {
+        if (selectedIndex === 5) {
+          setSelectedIndex(0);
+        } else {
+          setSelectedIndex(selectedIndex + 1);
+        }
+      }
+      if (keyName === 'ArrowUp') {
+        if (selectedIndex === 0) {
+          setSelectedIndex(5);
+        } else {
+          setSelectedIndex(selectedIndex - 1);
+        }
+      }
+    },
+    [selectedIndex]
+  );
+
+  const { keyName, keyCode, keyCodeHistory, keyNameHistory } = useKeyUp(
+    handleKeyUp
+  );
+  useKeyRepeat(handleKeyUp);
+
+  const getMenuItems = () => {
+    let menuItems = [];
+    for (let i = 0; i < 6; i++) {
+      menuItems.push(
+        <span
+          key={'item' + i}
+          style={{ color: selectedIndex === i ? 'green' : 'white' }}
+        >
+          Item {i}
+        </span>
+      );
+    }
+    return menuItems;
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Use up- and down arrow keys!</h1>
+        <h2>Example Menu</h2>
+        {getMenuItems()}
+        <h3>Last pressed key code: {keyCode}</h3>
+        <h3>Last pressed key name: {keyName}</h3>
+        <h6>
+          Key Code History: <br />
+          {keyCodeHistory.map(item => item + ', ')}
+        </h6>
+        <h6>
+          Key Name History:
+          <br />
+          {keyNameHistory.map(item => item + ', ')}
+        </h6>
       </header>
     </div>
   );
